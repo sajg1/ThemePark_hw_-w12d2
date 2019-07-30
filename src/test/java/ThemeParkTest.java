37a60a80d8a0fd8fbcd4a9dc2633d5f065443eab
Steve Gossip <stephengossip@gmail.com>
2019-07-30 22:17:59 +0100

    can add multiple stalls and attractions to IReviewed ArrayList

--- src/test/java/ThemeParkTest.java
+++ src/test/java/ThemeParkTest.java
@@ -2,6 +2,8 @@ import attractions.RollerCoaster;
 import behaviours.IReviewed;
 import org.junit.Before;
 import org.junit.Test;
+import stalls.IceCreamStall;
+import stalls.ParkingSpot;
 
 import java.util.ArrayList;
 
@@ -11,11 +13,14 @@ public class ThemeParkTest {
 
     ThemePark themePark;
     RollerCoaster rollercoaster;
+    IceCreamStall iceCreamStall;
+
 
     @Before
     public void before() {
         themePark = new ThemePark("Alton Towers");
         rollercoaster = new RollerCoaster("Inferno", 9);
+        iceCreamStall = new IceCreamStall("Joey's", "Joey", ParkingSpot.A1, 8);
     }
 
     @Test
@@ -34,7 +39,14 @@ public class ThemeParkTest {
         assertEquals(1, themePark.countReviews());
     }
 
-    
+    @Test
+    public void canAddMultipleDifferentReviewedLocations() {
+        themePark.addReview(rollercoaster);
+        themePark.addReview(iceCreamStall);
+        assertEquals(2, themePark.countReviews());
+    }
+
+
 
 
 
