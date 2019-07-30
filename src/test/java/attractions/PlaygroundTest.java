a3802c68bbfda327beb700bf609af40ae0a498da
Steve Gossip <stephengossip@gmail.com>
2019-07-30 16:48:57 +0100

    can allow a visitor 15 or under to enter the playground

--- src/test/java/attractions/PlaygroundTest.java
+++ src/test/java/attractions/PlaygroundTest.java
@@ -2,15 +2,20 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class PlaygroundTest {
     Playground playground;
+    Visitor visitor1;
+    Visitor visitor2;
 
     @Before
     public void setUp() throws Exception {
         playground = new Playground("Fun Zone", 7);
+        visitor1 = new Visitor(8, 1.0, 10.00);
+        visitor2 = new Visitor(16, 1.8, 20.00);
     }
 
     @Test
@@ -26,5 +31,11 @@ public class PlaygroundTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, playground.getVisitCount());
+    }
+
+    @Test
+    public void canAllowAccessIf15OrUnder() {
+        assertEquals(true, playground.isAllowedTo(visitor1));
+
     }
 }
